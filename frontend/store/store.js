a1bff249a56316c41f223867c4ab0106a9a5d3d9
eric <el.eberhart40@gmail.com>
2019-02-19 10:59:45 -0500

    style demoUser button

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
-// import rootReducer from '../reducers/root_reducer';
-import logger from 'redux-logger';
+import rootReducer from '../reducers/root_reducer';
+// import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 const configureStore = (preloadedState = {}) => {
